ca51801beb720aaed21e94c8ed7df379a04f3068
ArslanTech-dev <arslanchkpt@gmail.com>
2026-08-12 17:56:16 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    PREFIX: '.',
+    BOT_NAME: 'ARSLAN MD ULTRA',
+    BOT_LOGO: 'https://files.catbox.moe/0w1hu5.jpg',
+    OWNER: ['923084991001@s.whatsapp.net'],
+    OWNER_NAME: 'ARSLAN TECH\'S',
+    VERSION: '4.0.0',
+    PAIRING_NUMBER: '923084991001',   // your WhatsApp number (without @)
+    ANTI_CALL: true,
+    ANTI_DELETE: false,
+    AUTO_BLOCK_CALL: false,
+    CALL_MSG: 'Sorry, calls are not accepted. Please DM me.',
+};
